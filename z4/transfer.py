dee10fa5acf8d5195e79074c4d749ed4995d220e
Bodzio712 <bodzio_chomik@wp.pl>
2018-11-07 17:16:21 +0100

    Update transfer.py

--- z4/transfer.py
+++ z4/transfer.py
@@ -14,4 +14,13 @@ import jwt
 import datetime
 
 app = Flask(__name__)
+
 app.secret_key = b'35325fsdgsdg4gsd3fsge'
+jwt_secret_key =  '124hgjhghj214214124jj'
+
+r = redis.Redis()
+
+#Trasownik do pobierania plików
+@app.route('/pogodzip/login/userfiles/<path:file0>', methods=['GET', 'POST'])
+def download(file0):
+return send_from_directory(directory='userfiles', filename=file0)
