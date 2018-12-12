49e19eb9348591c859bc65b651d6fb3f6b4d578d
Piotr <ppogodzinski@vp.pl>
2018-12-12 23:09:36 +0100

    Auth0

---
+++ z7/dl.py
@@ -0,0 +1,79 @@
+# coding: utf-8
+
+from flask import Flask
+from flask import session
+from flask import request
+from flask import render_template
+from flask import redirect
+from flask import send_from_directory
+from flask import url_for
+
+import csv
+import os
+import uuid
+import redis
+import jwt
+import datetime
+
+app = Flask(__name__)
+
+app.secret_key = b'35325fsdgsdg4gsd3fsge'
+
+#Wczytywanie kluczy z pliku sec.cfg
+with open ('sec.cfg', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    for row in reader:
+        if row['key'] == 'client_secret':
+            auth_client_secret = row['value']
+        if row['key'] == 'jwt_secret_key':
+            jwt_secret_key = row['value']
+
+
+r = redis.Redis()
+
+#Trasownik do pobierania plików
+@app.route('/pogodzip/dl/userfiles/<path:file0>', methods=['GET', 'POST'])
+def download(file0):
+    token = request.form['token']
+    if checkToken(token):
+        return send_from_directory(directory='userfiles', filename=file0)
+    else:
+        return redirect('/pogodzip/webapp/logout')
+
+#Trasownik do metody obsługującej wysyłanie piku
+@app.route('/pogodzip/dl/uploading', methods=['POST'])
+def uploading():
+    if checkToken(request.form['token']):
+        user = getUser(token)
+        path = 'userfiles/' + str(user) + '/'
+        files = os.listdir('userfiles/' + str(user) + '/')
+        toUpload = request.files['file']
+        if len(files) <5:
+            toUpload.save(path + toUpload.filename)
+            return redirect('/pogodzip/webapp/home')
+        else:
+            return redirect('/pogodzip/webapp/home')
+    
+    return redirect('/pogodzip/webapp/login')
+
+#Wyciąganie danych z tokena
+def checkToken(token):
+    token_parts = {}
+    #Odkomentuj poniższą odpcję do debudowania (sprawdzanie czy token jest przesyłany)
+    #print(token)
+    try:
+        token_parts = jwt.decode(token, jwt_secret_key)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError) as e:
+        #False jesli wystąpi wyjątek (brak tokena lub token wygasł)
+        return False
+    return token_parts['login'] == str(r.hget('pogodzip:webapp:' + token_parts['sid'], 'login'))[2:-1]
+
+#Wyciąganie nazwy użytkownika
+def getUser(token):
+    token_parts = {}
+    try:
+        token_parts = jwt.decode(token, jwt_secret_key)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError) as e:
+        return False
+    return token_parts['login']
